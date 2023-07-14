1bc4850386893088302b8e7be28cc11a37b8e53b
palanjian <ppalanji@usc.edu>
2023-07-13 21:10:15 -0700

    CSS looking good

---
+++ public/js/index.js
@@ -0,0 +1,9 @@
+
+document.addEventListener("mousemove", parallax);
+function parallax(event) {
+    const sigma = 5;
+    const x = (window.innerWidth - event.pageX * sigma) / 90;
+    const y = (window.innerHeight - event.pageY * sigma) / 90;
+    console.log("x=" + x + "y=" + y)
+    document.getElementById('hero-img').style.transform = `translateX(${x}px) translateY(${y}px)`;
+}
