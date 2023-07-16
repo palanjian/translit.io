e825821a155d687fac76e50387d6a4588cdfeed9
palanjian <ppalanji@usc.edu>
2023-07-16 16:39:36 -0700

    Attempted to fix scrolling

--- public/js/index.js
+++ public/js/index.js
@@ -4,6 +4,6 @@ function parallax(event) {
     const sigma = 5;
     const x = (window.innerWidth - event.pageX * sigma) / 90;
     const y = (window.innerHeight - event.pageY * sigma) / 90;
-    console.log("x=" + x + "y=" + y)
+    //console.log("x=" + x + "y=" + y)
     document.getElementById('hero-img').style.transform = `translateX(${x}px) translateY(${y}px)`;
 }
