c0c015018707c209c8f3a2ec8f9bfab440983136
palanjian <ppalanji@usc.edu>
2023-08-03 19:39:59 -0700

    Added copy to clipboard functionality

--- public/js/index.js
+++ public/js/index.js
@@ -7,3 +7,11 @@ function parallax(event) {
     //console.log("x=" + x + "y=" + y)
     document.getElementById('hero-img').style.transform = `translateX(${x}px) translateY(${y}px)`;
 }
+
+function copyToClipboard(){
+    var text = document.getElementById('output');
+    text.select();
+    text.setSelectionRange(0, 99999); // For mobile devices
+     // Copy the text inside the text field
+    navigator.clipboard.writeText(text.value);
+}
